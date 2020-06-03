f9977df52df9316fdb62f568611c86ca073eec93
BuildTools <unconfigured@null.spigotmc.org>
2020-06-03 16:48:15 +0200

    fixing remaining tests after authentication intro

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,26 +1,34 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { AuthService } from './auth/auth.service';
+import { jwtConstants } from './auth/constants';
 import { User } from './users/user.entity';
 import { UserRepositoryMock } from './users/user.repository.mock';
+import { UsersService } from './users/users.service';
 
 describe('App Controller', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule, UsersModule],
+      //imports: [AuthModule, UsersModule],
       controllers: [AppController],
+      imports: [
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: { expiresIn: '1800s' },
+        }),
+      ],
       providers: [
         UsersService,
         {
           provide: getRepositoryToken(User),
           useClass: UserRepositoryMock,
         },
+        AuthService,
         AppService,
       ], 
     }).compile();
@@ -28,7 +36,7 @@ describe('App Controller', () => {
     appController = module.get<AppController>(AppController);
   });
 
-  xit('should be defined', () => {
+  it('should be defined', () => {
     expect(appController).toBeDefined();
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,7 +4,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { UserRepositoryMock } from '../users/user.repository.mock';
-import { UsersModule } from '../users/users.module';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
@@ -16,7 +15,6 @@ describe('AuthService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        UsersModule,
         PassportModule,
         JwtModule.register({
           secret: jwtConstants.secret,
@@ -37,7 +35,11 @@ describe('AuthService', () => {
     service = module.get<AuthService>(AuthService);
   });
 
-  xit('should be defined', () => {
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  xit('should be unit tested', () => {
     expect(service).toBeDefined();
   });
 });
