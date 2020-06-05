aefdc9cffdab8acb825a35e442868de913e0052e
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 15:01:21 +0200

    fixed quiz logic in bakend

--- src/app-configuration/app-configuration.service.ts
+++ src/app-configuration/app-configuration.service.ts
@@ -8,18 +8,25 @@ export class AppConfigurationService {
         switch (level) {
             case 1:
                 nextDueIn = 0;
+                break;
             case 2:
                 nextDueIn = 1;
+                break;
             case 3:
                 nextDueIn = 3;
+                break;
             case 4:
                 nextDueIn = 9;
+                break;
             case 5:
                 nextDueIn = 29;
+                break;
             case 6:
                 nextDueIn = 90;
+                break;
             default:
                 nextDueIn = 0;
+                break;
         }
         return nextDueIn;
     }
