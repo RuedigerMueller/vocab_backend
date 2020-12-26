11dd0ae3e417a235bf5884b656cf85e29bc0535d
RuedigerMueller <unconfigured@null.spigotmc.org>
2020-12-26 18:49:40 +0100

    added out of the box support for CF

--- src/db-configuration.ts
+++ src/db-configuration.ts
@@ -1,25 +1,17 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
-import { ConnectionOptions, getConnectionOptions } from "typeorm";
+import { getConnectionOptions } from "typeorm";
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
-    const entities = ['dist/**/*.entity.js'];
-    const synchronize = true;
-    const migrations = ['migration/*.js'];
-    const driver_extra = '{ "ssl": true, "rejectUnauthorized": false }';
-
-     const connectionOption: ConnectionOptions = await getConnectionOptions();
-     console.log(connectionOption);
-
      // if not running in Cloud Foundry take the connectionOpions as is which means
      // 1) from ENV variables => used when running in the Cloud
      // 2) from .env/ormconfig.json when running locally
      if(process.env.VCAP_SERVICES === undefined) {
-        return connectionOption;
+        return await getConnectionOptions();
      } else {
-        const vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+        const vcap_services = JSON.parse(process.env.VCAP_SERVICES);     
         return {
             type: 'postgres',
             host: vcap_services['postgresql-db'][0].credentials.hostname,
@@ -27,11 +19,15 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
             username: vcap_services['postgresql-db'][0].credentials.username,
             password: vcap_services['postgresql-db'][0].credentials.password,
             database: vcap_services['postgresql-db'][0].credentials.dbname,
-            url: vcap_services['postgresql-db'][0].credentials.dbname,
-            entities: entities,
-            synchronize: synchronize,
-            migrations: migrations,
-            extra: driver_extra,
+            url: vcap_services['postgresql-db'][0].credentials.uri,
+            entities: ['dist/**/*.entity.js'],
+            synchronize: true,
+            migrations: ['migration/*.js'],
+            extra: '{ "ssl": true, "rejectUnauthorized": true }',
+            ssl: {
+                ca: vcap_services['postgresql-db'][0].credentials.sslrootcert,
+                cert:vcap_services['postgresql-db'][0].credentials.sslcert,
+            }
           };
      }   
   }
